fc54bdd5ebb21eaface10096f31391f09a615c7f
Hand_MK <thflgg133@naver.com>
2022-06-20 02:27:30 +0900

    3

--- 숫자 카드 게임.py
+++ 숫자 카드 게임.py
@@ -1,23 +1,12 @@
 import sys
 from itertools import combinations
 
-'''
-N, K = map(int, sys.stdin.readline().split())
-
-arr = list(combinations([i % N for i in range(N)], K))
-ans = 0
-
-for i in range(len(arr)):
-    if sum(arr[i]) % N == 0:
-        ans += 1
-
-print(ans)
-'''
-
-N, K = map(int, sys.stdin.readline().split())
-
+N, K= map(int, sys.stdin.readline().split())
+    
 arr = [i for i in range(N)]
-print(arr)
+dp = [[0 for _ in range(N)] for _ in range(K+1)]
+print(dp)
 
-# Idea
-# 1. N값을 기준으로 배열안에 있는 수로 나올수 있는 나머지를 구한다??
+for i in range(N):
+    for j in range(K+1):
+        
