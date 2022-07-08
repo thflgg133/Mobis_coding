aea6f9b8082b628de1b2b73e014ee47831efc2f7
Hand_MK <thflgg133@naver.com>
2022-07-09 02:48:07 +0900

    Mobis

--- ADAS 시스템.py
+++ ADAS 시스템.py
@@ -6,6 +6,14 @@ def bfs(i,j):
     
     while queue:
         x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx_1[i]
+            ny = y + dy_1[i]
+            
+            if 0 <= nx < W and 0 <= ny < H:
+                pass
+
 
 
 
@@ -13,10 +21,13 @@ W, H = map(int, sys.stdin.readline().split())
 maps = [list(int, sys.stdin.readline().rstrip()) for _ in range(W)]
 visited =[[True] * H for _ in range(W)]
 
-dx = [-1,0,1,0]
-dy = [0,-1,0,1]
+dx_1 = [-1,0,1,0]
+dy_1 = [0,-1,0,1]
+
+dx_2 = [-1,-1,-1,0,0,1,1,1]
+dy_2 = [-1,0,1,-1,1,-1,0,1]
 
 for i in range(W):
     for j in range(H):
         if maps[i][j] == 'S':
-        bfs(i,j)
+            bfs(i,j)
